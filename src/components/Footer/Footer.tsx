f62ed808473f08f9f7fa3c066db437ef2c714223
Beginner-Project <boripats63@nu.ac.th>
2024-01-13 22:58:21 +0700

    feat(components): create footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import style from "/Footer.module.css";
+import style from "./Footer.module.css";
 const Footer: FC = () => {
   return <div className={style.Footer}></div>;
 };
