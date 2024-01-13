30dafa4e53da522c62711fe01d43062487181570
Beginner-Project <boripats63@nu.ac.th>
2024-01-13 22:54:54 +0700

    feat(pages): update font in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Home } from './Home/Home'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Home } from "./Home/Home";
+import { Roboto, Open_Sans } from "next/font/google";
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function App() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between${roboto.className}`}
     >
-      <Home/>
+      <Home />
     </main>
-  )
+  );
 }
