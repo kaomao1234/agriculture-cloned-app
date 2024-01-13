0f8de362e4963520c096170ef433648683e0c746
Beginner-Project <boripats63@nu.ac.th>
2024-01-13 22:53:19 +0700

    style(config): update font and theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss'
-
+const defaultTheme = require('tailwindcss/defaultTheme')
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +7,20 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'roboto': ['Roboto',
+        'sans-serif'],
+    },
+    colors: {
+      'transparent': 'transparent',
+      'blue_dianne': '#274C5B',
+      'acapulco': '#7EB693',
+      'sunset_pearl': '#EFD372',
+      'black_coral_pearl': '#525C60',
+      'alto': '#D4D4D4',
+      'alabaster': '#F9F8F8',
+      'saltpan': '#EFF6F1'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
