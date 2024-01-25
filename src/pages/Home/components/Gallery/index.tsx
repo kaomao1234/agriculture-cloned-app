00ac216f2ce5f975bfa2e7cbde0fd8318d47b027
Beginner-Project <boripats63@nu.ac.th>
2024-01-25 23:44:38 +0700

    feat(Home): create gallery

--- src/pages/Home/components/Gallery/index.tsx
+++ src/pages/Home/components/Gallery/index.tsx
@@ -1,5 +1,31 @@
 import { FC } from "react";
 import style from "./main.module.scss";
-export const Gallery: FC = () => {
-  return <div id={style.main}></div>;
+import { Button } from "@mui/material";
+interface galleryItem {
+  name: string;
+  imageSrc: string;
+}
+interface Props {
+  data: galleryItem[];
+}
+export const Gallery: FC<Props> = (props) => {
+  return (
+    <div id={style.main}>
+      {props.data.map((value, index) => {
+        return (
+          <div className="relative flex overflow-clip">
+            <div className="absolute z-10 flex size-full items-center justify-center">
+              <Button
+                variant="contained"
+                className="rounded-[20px] bg-white px-[41px] py-[17px] font-roboto text-xl font-medium capitalize text-blue_dianne hover:bg-white"
+              >
+                {value.name}
+              </Button>
+            </div>
+            <img src={value.imageSrc} alt="" />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
