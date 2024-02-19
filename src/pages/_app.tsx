5743e7457505b5e3a7c1cc583743d8bf1fe5d9fa
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-19 14:05:25 +0000

    feat(pages): styling breakpoint tracker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,24 +6,24 @@ import CircularProgress from "@mui/material/CircularProgress";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex h-full min-h-screen w-full flex-col">
+      <div className="flex flex-row gap-3 fixed z-30 bg-sunset_pearl w-full">
+        <label className="hidden text-xl sm:visible sm:flex sm:md:hidden">
+          Sm breakpoint-up
+        </label>
+        <label className="hidden text-xl md:visible md:flex md:lg:hidden">
+          Md breakpoint-up
+        </label>
+        <label className="hidden text-xl lg:visible lg:flex lg:xl:hidden">
+          Lg breakpoint-up
+        </label>
+        <label className="hidden text-xl xl:visible xl:flex xl:2xl:hidden">
+          Xl breakpoint-up
+        </label>
+        <label className="hidden text-xl 2xl:visible 2xl:flex">
+          2Xl breakpoint-up
+        </label>
+      </div>
       <div className="flex flex-col">
-        <div className="flex flex-row gap-3">
-          <label className="hidden text-xl sm:visible sm:flex sm:md:hidden">
-            Sm breakpoint-up
-          </label>
-          <label className="hidden text-xl md:visible md:flex md:lg:hidden">
-            Md breakpoint-up
-          </label>
-          <label className="hidden text-xl lg:visible lg:flex lg:xl:hidden">
-            Lg breakpoint-up
-          </label>
-          <label className="hidden text-xl xl:visible xl:flex xl:2xl:hidden">
-            Xl breakpoint-up
-          </label>
-          <label className="hidden text-xl 2xl:visible 2xl:flex">
-            2Xl breakpoint-up
-          </label>
-        </div>
         <Nav />
         <Component {...pageProps} />
         <Footer />
