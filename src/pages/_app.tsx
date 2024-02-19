37858449023591f9c01acb730dc73b300cdc9cdd
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-19 12:28:35 +0000

    feat: create breakpoint tracker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,28 @@ import CircularProgress from "@mui/material/CircularProgress";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex h-full min-h-screen w-full flex-col">
-      <div className="flex-col flex">
+      <div className="flex flex-col">
+        <div className="flex flex-row gap-3">
+          <label className="hidden text-xl sm:visible sm:flex sm:md:hidden">
+            Sm breakpoint-up
+          </label>
+          <label className="hidden text-xl md:visible md:flex md:lg:hidden">
+            Md breakpoint-up
+          </label>
+          <label className="hidden text-xl lg:visible lg:flex lg:xl:hidden">
+            Lg breakpoint-up
+          </label>
+          <label className="hidden text-xl xl:visible xl:flex xl:2xl:hidden">
+            Xl breakpoint-up
+          </label>
+          <label className="hidden text-xl 2xl:visible 2xl:flex">
+            2Xl breakpoint-up
+          </label>
+        </div>
         <Nav />
         <Component {...pageProps} />
         <Footer />
       </div>
-      {/* <div className="flex h-full min-h-screen w-full flex-col items-center justify-center gap-2 xl:hidden">
-        <label className="font-yellowtail text-2xl text-sunset_pearl">
-          Open this web with desktop resolution
-        </label>
-        <CircularProgress className="text-sunset_pearl" />
-      </div> */}
     </div>
   );
 }
