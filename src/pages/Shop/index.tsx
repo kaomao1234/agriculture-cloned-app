1b694a4b830eaaa34a8112f8af7c04f6add7a6c0
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-20 13:55:16 +0000

    feat(pages): apply viewmodel into shop page for show content of shop component

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,13 +1,18 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import style from "./Shop.module.scss";
 import { Banner, Newslatteer } from "@/components";
-const Shop: FC = () => {
+import { Shop } from "@/templates/Shop";
+import { ShopViewModel } from "@/viewmodels";
+const ShopPage: FC = () => {
+  const viewModelRef = useRef(new ShopViewModel());
+  const viewModel = viewModelRef.current
   return (
     <div id={style.main}>
       <Banner image="Shop/Banner/Image.png" label="Shop" />
+      <Shop productData={viewModel.productItemCard}/>
       <Newslatteer/>
     </div>
   );
 };
 
-export default Shop;
+export default ShopPage;
