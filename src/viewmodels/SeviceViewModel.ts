01083d96fef5830f0ce68cc7395e528da5bf74ef
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-22 15:55:18 +0000

    feat(viewmodels): create service view model.

---
+++ src/viewmodels/SeviceViewModel.ts
@@ -0,0 +1,30 @@
+export class ServiceViewModel {
+    serviceLeftData = [
+        {
+            image: "Service/Service/Milk Bottle.png",
+            title: "Dairy Products"
+        },
+        {
+            image: "Service/Service/Online Store.png",
+            title: "Store Services"
+        },
+        {
+            image: "Service/Service/Delivery.png",
+            title: "Delivery Services"
+        }
+    ];
+    serviceRightData = [
+        {
+            image: "Service/Service/Natural Food.png",
+            title: "Agricultural Services"
+        },
+        {
+            image: "Service/Service/Vegetables Bag.png",
+            title: "Organic Products"
+        },
+        {
+            image: "Service/Service/Radish.png",
+            title: "Fresh Vegetables"
+        }
+    ];
+};
--- src/viewmodels/index.ts
+++ src/viewmodels/index.ts
@@ -1,3 +1,4 @@
+export { ServiceViewModel } from "./SeviceViewModel";
 export { ShopSingleViewModel } from "./ShopSingleViewModel";
 export { ShopViewModel } from "./ShopViewModel";
 export { HomeViewModel } from "./HomeViewModel";
