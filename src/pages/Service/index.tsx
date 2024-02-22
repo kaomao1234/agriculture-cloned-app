5ed1834c488ce4162e4f34cc301e579b3aee84eb
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-22 15:52:39 +0000

    feat(pages): provide and use of viewmodel to service page.

--- src/pages/Service/index.tsx
+++ src/pages/Service/index.tsx
@@ -1,12 +1,18 @@
 import { Banner } from "@/components";
 import { Service, Videos } from "@/templates/Service";
-import { FC } from "react";
+import { ServiceViewModel } from "@/viewmodels";
+import { FC, useRef } from "react";
 
 const ServicePage: FC = () => {
+  const viewModelRef = useRef(new ServiceViewModel());
+  const viewModel = viewModelRef.current;
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mb-14">
       <Banner image="Service/Banner/image.png" label="Services" />
-      <Service />
+      <Service
+        serviceLeftData={viewModel.serviceLeftData}
+        serviceRightData={viewModel.serviceRightData}
+      />
       <Videos />
     </div>
   );
