5ae4ba48acb6797bb54e3e0dd8e387ccb7a1bf41
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-22 15:54:17 +0000

    feat(templates): create props and use to  show as component.

--- src/templates/Service/Service/index.tsx
+++ src/templates/Service/Service/index.tsx
@@ -1,10 +1,19 @@
 import { CustomOutlinedButton } from "@/components";
+import { ViewAgenda } from "@mui/icons-material";
 import { FC } from "react";
 
-const Service: FC = () => {
+interface serviceData {
+  image: string;
+  title: string;
+}
+
+const Service: FC<{
+  serviceLeftData: serviceData[];
+  serviceRightData: serviceData[];
+}> = (props) => {
   return (
-    <div className="flex w-full flex-col items-center bg-alabaster">
-      <div className="flex flex-col w-fit">
+    <div className="flex w-full flex-col items-center bg-alabaster gap-y-5 py-10">
+      <div className="flex w-fit flex-col">
         <div className="font-yellowtail text-2xl font-normal text-acapulco">
           What we Grow
         </div>
@@ -13,7 +22,47 @@ const Service: FC = () => {
           <br /> Better Future
         </div>
       </div>
-      <div className="grid grid-cols-3"></div>
+      <div className="grid-cols-3 items-center grid gap-x-12 xl:px-[10%] lg:px-[5%]">
+        <div className="flex flex-col gap-y-5">
+          {props.serviceLeftData.map((val, index) => {
+            return (
+              <div
+                className="flex flex-col items-end gap-2 text-right"
+                key={index}
+              >
+                <img src={val.image} className="size-10 " />
+                <label className="font-roboto text-base font-medium text-blue_dianne">
+                  {val.title}
+                </label>
+                <label className="font-sans text-sm font-normal text-black_coral_pearl">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptat
+                  accusantium doloremqlaudantium. Sed ut perspiciatis
+                </label>
+              </div>
+            );
+          })}
+        </div>
+        <img src="Service/Service/photo.png" className="w-full" />
+        <div className="flex flex-col gap-y-5">
+          {props.serviceRightData.map((val, index) => {
+            return (
+              <div
+                className="flex flex-col items-start gap-2 text-left"
+                key={index}
+              >
+                <img src={val.image} className="size-10" />
+                <label className="font-roboto text-base font-medium text-blue_dianne">
+                  {val.title}
+                </label>
+                <label className="font-sans text-sm font-normal text-black_coral_pearl">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptat
+                  accusantium doloremqlaudantium. Sed ut perspiciatis
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <CustomOutlinedButton text="Explore More" />
     </div>
   );
