863b6dbc1709b5e377dc1fb612b7973ebcea02e8
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-24 11:05:41 +0000

    feat(pages): use viewmodel in portfolio page

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,19 +1,24 @@
 import { Banner, Newslatteer } from "@/components";
 import { Portfolio } from "@/templates/Portfolio";
-import { FC } from "react";
+import { PortfolioViewModel } from "@/viewmodels";
+import { useRouter } from "next/router";
+import { FC, useRef } from "react";
 
 let a = 0;
-const recur = ()=>{
-    console.log(a);
-    a+=1;
-    return <div className="bg-sunset_pearl">{a < 41674 ? recur():null}</div>
-}
+const recur = () => {
+  console.log(a);
+  a += 1;
+  return <div className="bg-sunset_pearl">{a < 41674 ? recur() : null}</div>;
+};
 
 const PortfolioPage: FC = () => {
+  const router = useRouter();
+  const viewModelRef = useRef(new PortfolioViewModel(router));
+  const viewModel = viewModelRef.current;
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <Banner image="Portfolio/Banner/image.png" label="portfolio standard" />
-      <Portfolio/>
+      <Portfolio portFolioItemData={viewModel.portFolioItemData} />
       <Newslatteer />
     </div>
   );
