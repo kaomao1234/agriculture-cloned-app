5d66a3a1e1efaf8e08d2dfd38e498f708da53655
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-25 12:51:20 +0000

    feat(components): update style in newslatteer.

--- src/components/Newslatteer/index.tsx
+++ src/components/Newslatteer/index.tsx
@@ -3,31 +3,33 @@ import { FC } from "react";
 import Image from "next/image";
 export const Newslatteer: FC = () => {
   return (
-    <div className="relative mx-[15%] my-[5%] overflow-clip rounded-3xl ">
-      <div className="absolute z-0 flex size-full items-center justify-center">
-        <div className="flex w-full flex-row justify-between px-6">
-          <div className="font-roboto text-3xl font-extrabold text-white">
-            Subscribe to
-            <br />
-            our Newsletter
-            <br />
-          </div>
-          <div className="flex flex-row gap-2">
-            <input
-              id="outlined-basic"
-              placeholder="Your Email Address"
-              className="h-fit rounded-2xl bg-white px-6 py-7 font-roboto text-lg text-black_coral_pearl outline-none"
-            />
-            <Button
-              variant="contained"
-              className="rounded-2xl bg-blue_dianne px-12 py-7 font-roboto text-xl font-bold capitalize hover:bg-blue_dianne"
-            >
-              Subscribe
-            </Button>
+    <div className="flex items-center justify-center">
+      <div className="relative mx-[15%] my-[5%] w-[60%] overflow-clip rounded-3xl">
+        <div className="absolute z-0 flex size-full items-center justify-center">
+          <div className="flex w-full flex-row justify-between items-center px-6">
+            <div className="font-roboto text-2xl font-extrabold text-white">
+              Subscribe to
+              <br />
+              our Newsletter
+              <br />
+            </div>
+            <div className="flex flex-row gap-2">
+              <input
+                id="outlined-basic"
+                placeholder="Your Email Address"
+                className="h-fit rounded-2xl bg-white px-6 py-7 font-roboto text-base text-black_coral_pearl outline-none"
+              />
+              <Button
+                variant="contained"
+                className="rounded-2xl bg-blue_dianne px-12 py-7 font-roboto text-base font-bold capitalize hover:bg-blue_dianne"
+              >
+                Subscribe
+              </Button>
+            </div>
           </div>
         </div>
+        <img alt="" src="/Newslatteer/Photo.jpg" className="" />
       </div>
-      <img alt="" src="/Newslatteer/Photo.jpg"className="" />
     </div>
   );
 };
