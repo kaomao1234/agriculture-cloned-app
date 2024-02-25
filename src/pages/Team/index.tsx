0f61e08ecd8cece1bcd2bc6417fe5f1e53f94744
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-25 12:53:02 +0000

    feat(pages): use viewmodel in team page.

--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -1,12 +1,17 @@
 import { Banner, Newslatteer } from "@/components";
 import { Team } from "@/templates/Team";
-import { FC } from "react";
+import { TeamViewModel } from "@/viewmodels";
+import { useRouter } from "next/router";
+import { FC, useRef } from "react";
 
 const TeamPage: FC = () => {
+  const router = useRouter();
+  const viewModelRef = useRef(new TeamViewModel(router));
+  const viewModel = viewModelRef.current;
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <Banner image="Team/Banner/Image.jpg" label="Our Team" />
-      <Team />
+      <Team data={viewModel.employees} />
       <Newslatteer />
     </div>
   );
