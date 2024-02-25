31d5fe7527da178ee944884441faa89393a1eb55
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-25 12:56:38 +0000

    feat(templates): move dummy data to viewmodel instead and creat props to recieve data.

--- src/templates/Team/Team/index.tsx
+++ src/templates/Team/Team/index.tsx
@@ -2,45 +2,10 @@ import { EmployeeCard } from "@/components";
 import { EmployeeCardModel } from "@/models";
 import { FC } from "react";
 
-const Team: FC = () => {
-  const employees: EmployeeCardModel[] = [
-    {
-      name: "Giovani Bacardo",
-      role: "farmer",
-      links: ["facebook", "twitter"],
-      image: "Team/Team/person_1.jpg",
-    },
-    {
-      name: "Marianne Loreno",
-      role: "designer",
-      links: ["ig", "facebook", "twitter"],
-      image: "Team/Team/person_2.jpg",
-    },
-    {
-      name: "Riga Pelore",
-      role: "farmer",
-      links: ["ig", "facebook", "twitter"],
-      image: "Team/Team/person_3.jpg",
-    },
-    {
-      name: "Keira Knightley",
-      role: "farmer",
-      links: ["facebook", "twitter"],
-      image: "Team/Team/person_4.jpg",
-    },
-    {
-      name: "Scott Lawrence",
-      role: "designer",
-      links: ["ig", "facebook", "twitter"],
-      image: "Team/Team/person_5.jpg",
-    },
-    {
-      name: "Karen Allen",
-      role: "farmer",
-      links: ["ig", "facebook", "twitter"],
-      image: "Team/Team/person_6.jpg",
-    },
-  ];
+const Team: FC<{
+  data:EmployeeCardModel[]
+}> = (props) => {
+  
   return (
     <div className="mt-[5%] flex flex-col items-center bg-white px-[10%]">
       <div className="mb-5 size-full flex-col items-center justify-start gap-4 text-center">
@@ -59,7 +24,7 @@ const Team: FC = () => {
         </label>
       </div>
       <div className="mt-5 grid grid-cols-3 w-[85%] gap-10">
-        {employees.map((val, index) => {
+        {props.data.map((val, index) => {
           return <EmployeeCard key={index} {...val} />;
         })}
       </div>
