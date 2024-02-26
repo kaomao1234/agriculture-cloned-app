69b7f94e2d72fed6a57e5a099f57b405af223c11
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2024-02-26 09:15:51 +0000

    cerate contact page and component.

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,13 +1,14 @@
 import { Banner, Newslatteer } from "@/components";
-import { ContentOne, ContentTwo } from "@/templates/Contact";
+import { ContentOne, ContentTwo,EmailForm } from "@/templates/Contact";
 import { FC } from "react";
 
 const ContactPage:FC = ()=>{
     return <div className="flex flex-col min-h-screen bg-white">
         <Banner label="contact us" image="Contact/Banner/image.jpg"/>
-        <div className="flex flex-col px-[10%] py-[10%] gap-5">
+        <div className="flex flex-col xl:px-[10%] py-[5%] gap-10 px-[5%]">
         <ContentOne/>
         <ContentTwo/>
+        <EmailForm/>
         <Newslatteer/>
         </div>
     </div>
--- src/templates/Contact/EmailForm/index.tsx
+++ src/templates/Contact/EmailForm/index.tsx
@@ -1,6 +1,28 @@
 import { FC } from "react";
-
+import { Button } from '@mui/material';
 const EmailForm: FC = () => {
-  return <div className="flex flex-col xl:flex-row"></div>;
+  return <form className="grid grid-cols-1 lg:grid-cols-2 gap-10 px-[8%]">
+    <div className="flex flex-col justify-start gap-5">
+      <label className="text-blue_dianne text-base font-semibold font-robto capitalize">Full Name</label>
+      <input type="email" className="text-black_coral_pearl outline-none border-acapulco border rounded-xl p-4 italic" placeholder="Your Name" />
+    </div>
+    <div className="flex flex-col justify-start gap-5">
+      <label className="text-blue_dianne text-base font-semibold font-robto capitalize">Your Email</label>
+      <input type="email" className="text-black_coral_pearl outline-none border-acapulco border rounded-xl p-4 italic" placeholder="example@yourmail.com" />
+    </div>
+    <div className="flex flex-col justify-start gap-5">
+      <label className="text-blue_dianne text-base font-semibold font-robto capitalize">company</label>
+      <input type="email" className="text-black_coral_pearl outline-none border-acapulco border rounded-xl p-4 italic" placeholder="yourcompany name here" />
+    </div>
+    <div className="flex flex-col justify-start gap-5">
+      <label className="text-blue_dianne text-base font-semibold font-robto capitalize">Subject</label>
+      <input type="email" className="text-black_coral_pearl outline-none border-acapulco border rounded-xl p-4 italic" placeholder="how can we help" />
+    </div>
+    <div className="flex flex-col justify-start gap-5 col-span-2">
+      <label className="text-blue_dianne text-base font-semibold font-robto capitalize">Message</label>
+      <textarea  className="text-black_coral_pearl outline-none border-acapulco border rounded-xl p-4 italic h-[250px]" placeholder="hello there,i would like to talk about how to..." />
+    </div>
+      <Button variant="contained" className="text-white text-base font-bold font-roboto bg-blue_dianne rounded-xl capitalize w-fit px-12 py-7">Send message</Button>
+  </form>;
 };
 export { EmailForm };
