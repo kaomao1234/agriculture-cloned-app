c1b8ebf40100051061aa6f303900d64bc0c84d3a
Beginner-Project <boripats63@nu.ac.th>
2024-02-28 19:35:03 +0700

    feat(pages): create 404 page.

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,6 +1,10 @@
+import { Banner } from "@/templates/404";
 import {FC} from "react";
 const Custom404Page:FC = ()=>{
-    return <div></div>
+    return <div className="flex">
+        <Banner/>
+        
+    </div>
 }
 
 export default Custom404Page;
